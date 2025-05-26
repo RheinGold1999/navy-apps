e9a0a4aeac3bb852ff6788b5be2a5f167f70d036
Rui Zhang <rui.zhang@aigcic.com>
2025-05-26 17:55:19 +0800

    update crt0.c

--- libs/libos/src/crt0/crt0.c
+++ libs/libos/src/crt0/crt0.c
@@ -8,10 +8,17 @@ void call_main(uintptr_t *args) {
   // char *empty[] =  {NULL };
   // environ = empty;
   // exit(main(0, empty, empty));
+
+  // printf("args: %p\n", args);
   int argc = *((int *)args);
   char **argv = (char **)(args + 1);
   char **envp = (char **)(args + 1 + argc + 1);
   environ = envp;
+  // printf("argc: %d\n", argc);
+  // for (int i = 0; i < argc; ++i) {
+  //   printf("argv[%d]: %s\n", i, argv[i]);
+  // }
+  // printf("argv[%d]: %s\n", argc, argv[argc]);
   exit(main(argc, argv, envp));
   assert(0);
 }
